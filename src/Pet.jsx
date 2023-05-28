e9f56795ea0311026125a85eae9b2ffd2a9b403d
lidankatzav <lidankatzav5@gmail.com>
2023-05-28 14:33:02 +0300

    adding router & react-query

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,20 +1,22 @@
+import {Link} from 'react-router-dom';
+
 const Pet = ({name, animal, breed, images, city, state, id}) => {
     let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
     if (images.length > 0) {
         hero = images[0];
     }
     return (
-        <a href = {`/details/${id}`} className="pet">
+        <Link to = {`/details/${id}`} className="pet">
             <div className = "image-container">
                 <img src={hero} alt={name} />
             </div>
             <div className = "info">
                 <h1>{name}</h1>
                 <h2>
-                {`${animal} — ${breed} — ${city}, ${state}`}
+                {`${id} - ${animal} — ${breed} — ${city}, ${state}`}
                 </h2>
             </div>
-        </a>
+        </Link>
     );
 }
 
